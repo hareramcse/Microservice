81d91fecb453cba14b90f7c62b57d9cec13c4a53
Hareram Singh <harsingh18@publicisgroupe.net>
2018-10-18 07:24:40 +0530

    adding git local server

--- spring-cloud-config-server/src/main/java/com/hs/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/hs/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.in28minutes.microservices.springcloudconfigserver;
+package com.hs.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring-cloud-config-server/src/test/java/com/hs/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
+++ spring-cloud-config-server/src/test/java/com/hs/microservices/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.in28minutes.microservices.springcloudconfigserver;
+package com.hs.microservices.springcloudconfigserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
