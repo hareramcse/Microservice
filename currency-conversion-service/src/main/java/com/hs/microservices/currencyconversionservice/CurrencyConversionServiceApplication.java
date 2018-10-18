e879579577f94d727f2fec927a43495c2e1d8bca
Hareram Singh <harsingh18@publicisgroupe.net>
2018-10-18 19:38:29 +0530

    adding support for zull server

--- currency-conversion-service/src/main/java/com/hs/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/hs/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -6,12 +6,11 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients(basePackages="com.hs.microservices.currencyconversionservice")
+@EnableFeignClients(basePackages = "com.hs.microservices.currencyconversionservice")
 @EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
 	}
-
 }
